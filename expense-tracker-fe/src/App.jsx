6ce2a7d6f04ff9fe04c3597241d846922d59c65b
Aniket Singh <aniketsingh90126@gmail.com>
2026-06-23 19:51:40 +0530

    feat: login page

--- expense-tracker-fe/src/App.jsx
+++ expense-tracker-fe/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Dashboard from './pages/Dashboard/Dashboard'
 import Transactions from './pages/Transactions/Transactions'
+import Login from './pages/Login/Login'
 
 function App() {
   useEffect(() => {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/transactions" element={<Transactions />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
